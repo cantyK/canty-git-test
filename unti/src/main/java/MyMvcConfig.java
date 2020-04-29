0eb7e5dc073ab39d0d00a1e2418b88daf003f932
cantyK <2474611974@qq.com>
2020-04-29 10:36:09 +0800

    添加java文件

---
+++ unti/src/main/java/MyMvcConfig.java
@@ -0,0 +1,19 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class MyMvcConfig {
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+}
---
+++ unti/target/classes/views/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lxy
+  Date: 2020/4/28
+  Time: 10:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0.1 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
+    <title>Insert Title here</title>
+</head>
+<body>
+ <pre>
+     Welcome to Spring MVC world！！
+ </pre>
+</body>
+</html>
